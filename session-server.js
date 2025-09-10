cefa418f06ee393243aa5617d2390a57c9d865d9
Utkarsh Sinha <utkarshsinha@Utkarshs-MacBook-Air-2.local>
2025-09-11 03:07:17 +0530

    Serve index.html at root URL

--- session-server.js
+++ session-server.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(__dirname));
 
+// Serve index.html at root
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 // In-memory storage: sessions and questions
 const sessions = {};
 
